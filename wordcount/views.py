6cc6b6cf99a52d08becb9d8873fb906499064c4a
Russell Brown <russellbrown@Russells-MBP.attlocal.net>
2020-07-25 19:48:57 -0500

    Wu Tang is forever

---
+++ wordcount/views.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+def home(request):
+    return render(request, 'home.html')
+
+def count(request):
+    fulltext = request.GET['fulltext']
+
+    wordlist = fulltext.split()
+
+    worddictionary = {}
+
+    for word in wordlist:
+        if word in worddictionary:
+            #Increase
+            worddictionary[word] += 1
+        else:
+            #add to dictionary
+            worddictionary[word] = 1
+
+    return render(request, 'count.html', {'fulltext':fulltext, 'count':len(wordlist)})
+
+def about(request):
+    return render(request, 'about.html')
